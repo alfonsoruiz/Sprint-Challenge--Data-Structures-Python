848583e48105a3bb2a2e1509eb1ca37ae71c52db
alfonsoruiz <1800ruiz@gmail.com>
2020-08-02 21:11:04 -0700

    Implement Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.data = []
+        self.loc = 0
 
     def append(self, item):
-        pass
+        if len(self.data) < self.capacity:
+            self.data.append(item)
+            self.loc += 1
+        elif self.loc >= self.capacity:
+            self.data.pop(0)
+            self.data.insert(0, item)
+            self.loc = 1
+        else:
+            self.data.pop(self.loc)
+            self.data.insert(self.loc, item)
+            self.loc += 1
 
     def get(self):
-        pass
+        return self.data
